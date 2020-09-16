af43f6bb802547ffc2cd3cdfc13c901391165a49
vaughan <vaughan_93@hotmail.com>
2020-09-16 19:35:24 +1000

    added module 2 one thing wrong

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestions.js'
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
-    constructor(props){
+
+class Quiz extends Component {
+    constructor(props) {
         super(props);
-        this.state = {quiz_position: 1}
+        this.state = { quiz_position: 1 }
     }
-    render(){
-    return <div>
-        <div className='QuizQuestion'>
-                {quizData.quiz_questions[0].instruction_text}
-        </div>
-    </div>;
+    render() {
+        return
+        // <Avatar user={props.user} />
+        <QuizQuestion quiz_question={quizData.quiz_questions[quiz_position - 1]}/>
     }
 }
 
