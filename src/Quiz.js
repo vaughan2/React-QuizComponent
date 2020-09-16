f97f49d6afb0e0a1de2166336fa262416e9742a0
vaughan <vaughan_93@hotmail.com>
2020-09-16 17:28:22 +1000

    module 1 done

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component{
     render(){
     return <div>
         <div className='QuizQuestion'>
-                quizData
+                {quizData.quiz_questions[0].instruction_text}
         </div>
     </div>;
     }
