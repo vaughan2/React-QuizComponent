db96d118090073378e3b17b601d8aff70eb6b88a
vaughan <vaughan_93@hotmail.com>
2020-09-17 14:34:28 +1000

    update but its wrong

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { quiz_position: 1 }
-  }
-  render() {
-    return (
-      <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = { quiz_position: 1 }
+    }
+    render() {
+        const isQuizEnd = false
+        return (
+            <div>
+                if (isQuizEnd) {
+                    <QuizEnd />
+                }else{
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                }
+            </div>
+        )
+    }
 }
 
 export default Quiz 
